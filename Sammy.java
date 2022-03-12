85c0d797ef7a7a0b26abe0ea2f79c055b3877c77
Mok2304 <83498595+Mok2304@users.noreply.github.com>
2022-03-12 17:37:24 +0530

    Add files via upload

---
+++ Sammy.java
@@ -0,0 +1,17 @@
+class Sammy
+{
+  final int a=10;
+     void show()
+     {
+       
+       System.out.print(a);
+     }
+public static void main(String args[])
+  {
+     Sammy s1=new Sammy();
+     s1.show();
+     s1.show();
+     s1.show();
+  }
+}
+ 
